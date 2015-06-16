4533cb65e2b538fcfad6db77035d5390aa5714e2
Rafael Garcia <rafael.garcia@quadev.mx>
2015-06-15 22:07:24 -0400

    go to currentlocation

--- app/src/main/java/com/example/raf0c/basicmapsdisplay/MapsActivity.java
+++ app/src/main/java/com/example/raf0c/basicmapsdisplay/MapsActivity.java
@@ -1,22 +1,59 @@
 package com.example.raf0c.basicmapsdisplay;
 
+import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.provider.Settings;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity {
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
-    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+public class MapsActivity extends FragmentActivity implements View.OnClickListener {
+
+    private GoogleMap mMap;
+    Marker mCurrent;
+    LatLng mPosition;
+    boolean mCenterOnLocation;
+
+    private Button btnFindMe = null;
+    private EditText editLocation = null;
+    private ProgressBar probar =null;
+    private LocationManager locationMangaer = null;
+    private LocationListener locationListener = null;
+    private Boolean gpsPrendido = false;
+    private static final String TAG = "Consola : ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
+        initElements();
     }
 
     @Override
@@ -25,21 +62,6 @@ public class MapsActivity extends FragmentActivity {
         setUpMapIfNeeded();
     }
 
-    /**
-     * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
-     * installed) and the map has not already been instantiated.. This will ensure that we only ever
-     * call {@link #setUpMap()} once when {@link #mMap} is not null.
-     * <p/>
-     * If it isn't installed {@link SupportMapFragment} (and
-     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
-     * install/update the Google Play services APK on their device.
-     * <p/>
-     * A user can return to this FragmentActivity after following the prompt and correctly
-     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
-     * have been completely destroyed during this process (it is likely that it would only be
-     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
-     * method in {@link #onResume()} to guarantee that it will be called.
-     */
     private void setUpMapIfNeeded() {
         // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
@@ -52,14 +74,206 @@ public class MapsActivity extends FragmentActivity {
         }
     }
 
-    /**
-     * This is where we can add markers or lines, add listeners or move the camera. In this case, we
-     * just add a marker near Africa.
-     * <p/>
-     * This should only be called once and when we are sure that {@link #mMap} is not null.
-     */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(33.93713943,-84.36780809 )).title("Appartment"));
-        mMap.getMaxZoomLevel();
+
+
     }
+
+    @SuppressWarnings("deprecation")
+    private void initElements(){
+
+        probar              = (ProgressBar) findViewById(R.id.proBar);
+        editLocation        = (EditText) findViewById(R.id.eTlocation);
+        btnFindMe           = (Button) findViewById(R.id.btnFindme);
+        locationMangaer     = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);//set activity on portrait only
+        btnFindMe.setOnClickListener(this);
+        /*
+        * Initialize Progress Bar not Visible, when the user interacts, make it visible
+        * */
+        probar.setVisibility(View.INVISIBLE);
+        }
+
+    /*
+    * Check if Gps is on or not
+    * */
+    @SuppressWarnings("deprecation")
+    private Boolean gpsProvider() {
+
+        ContentResolver contentResolver = getBaseContext().getContentResolver();
+
+        /*
+        * Method deprecated, ask David for a better option
+        * */
+        boolean gpsStatus = Settings.Secure.isLocationProviderEnabled(contentResolver, LocationManager.GPS_PROVIDER);
+
+        if (gpsStatus)
+            return true;
+        else
+            return false;
+
+    }
+
+
+    /*
+       * We override the method onClick from the interface OnClickListener
+       * */
+    @Override
+    public void onClick(View v) {
+
+        gpsPrendido = gpsProvider();
+
+        if (gpsPrendido) {
+            /*
+            * Set ProgressBar to Visible
+            * */
+            probar.setVisibility(View.VISIBLE);
+
+            //Creates a locationlistener object
+            locationListener = new MyLocationListener();
+
+            //Use requestLocationUpdates method passing the provider, the interval time, the minimum distance and the object listener of the class implementing locationlistener
+            locationMangaer.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
+
+            mCenterOnLocation = true;
+
+
+        } else {
+            alertbox("GPS", "Please tur on your gsp");
+        }
+
+    }
+
+    /*
+    * Method to create a Custom Alert
+    * */
+    protected void alertbox(String title, String mymessage) {
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setMessage("NO GPS Available").setCancelable(false).setTitle("GPS").setPositiveButton("Gps On", new DialogInterface.OnClickListener() {
+
+            /*
+            * Custome ONCLICK
+            * */
+            public void onClick(DialogInterface dialog, int id) {
+
+                Intent myIntent = new Intent(Settings.ACTION_SECURITY_SETTINGS);
+                startActivity(myIntent);
+                dialog.cancel();
+
+            }
+        }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.cancel();
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    /*
+     * Classe to find the coordinates given, and showing the name of the city,
+      * What this does is to get the coordinates with the parameter loc
+      * then creates a Geocoder to translates the coordinates in the possible Address
+      * Then we have a List of Address, this array is being feed with all the translates that the geocoder is making, then,
+      * basing on this list it is filled with the most accurate address to the less accurate and takes the first one as being the best one.
+     * */
+    private class MyLocationListener implements LocationListener {
+
+
+        @Override
+        public void onLocationChanged(Location loc) {
+
+            editLocation.setText("");
+            probar.setVisibility(View.INVISIBLE);
+
+
+            Toast.makeText(getBaseContext(), "You moved! you are now in : Latitud: " + loc.getLatitude() + " Longitud: " + loc.getLongitude(), Toast.LENGTH_SHORT).show();
+            String longitude = "Longitud: " + loc.getLongitude();
+            Log.v(TAG, longitude);
+            String latitude = "Latitud: " + loc.getLatitude();
+            Log.v(TAG, latitude);
+            mPosition = new LatLng(loc.getLatitude(), loc.getLongitude());
+
+
+            /*
+            * We create a Geocoder, the geocoder will be able to translates the coordinates
+            * */
+            String city = null;
+            Geocoder gcd = new Geocoder(getBaseContext(), Locale.getDefault());
+            List<Address> addresses;
+
+            try {
+                addresses = gcd.getFromLocation(loc.getLatitude(), loc.getLongitude(), 1);
+
+                if (addresses.size() > 0) {
+                    System.out.println(addresses.get(0).getLocality());
+                }
+
+                city = addresses.get(0).getLocality();
+
+            } catch (IOException e) {
+
+                e.printStackTrace();
+
+            }
+
+            String s = longitude + "\n" + latitude;
+            editLocation.setText(s);
+            mCurrent = mMap.addMarker(new MarkerOptions().position(mPosition).title("You are here !"));
+
+
+            mCenterOnLocation = true;
+
+            centerOnLocation(mPosition, new GoogleMap.CancelableCallback() {
+                @Override
+                public void onFinish() {
+
+                }
+
+                @Override
+                public void onCancel() {
+                }
+            });
+
+        }
+
+
+        public void centerOnLocation(LatLng position, GoogleMap.CancelableCallback callback) {
+
+            if (mCenterOnLocation && mMap != null) {
+                mCenterOnLocation = false;
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(position, 12), 1500, callback);
+            }
+        }
+
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            // TODO Auto-generated method stub
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            // TODO Auto-generated method stub
+        }
+
+        @Override
+        public void onStatusChanged(String provider,
+                                    int status, Bundle extras) {
+            // TODO Auto-generated method stub
+        }
+    }
+
+
+    public GoogleMap getMap() {
+        if (mMap == null)
+            mMap = ((com.google.android.gms.maps.SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+        return mMap;
+    }
+
 }
